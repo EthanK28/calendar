aa1afe926f823bbcffe9110156faf2989465da5b
EthanK28 <kes0228@gmail.com>
2014-11-21 13:05:26 +0900

    Finished

--- src/main/java/com/mycompany/calendar/HomeController.java
+++ src/main/java/com/mycompany/calendar/HomeController.java
@@ -1,18 +1,30 @@
 package com.mycompany.calendar;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 import com.mycompany.calendar.dao.CalendarUserDao;
 import com.mycompany.calendar.dao.EventAttendeeDao;
 import com.mycompany.calendar.dao.EventDao;
+import com.mycompany.calendar.domain.Event;
+import com.mycompany.calendar.domain.EventAttendee;
+import com.mycompany.calendar.domain.EventLevel;
+import com.mycompany.calendar.domain.CalendarUser;
+import com.mycompany.calendar.service.CalendarService;
 
 /**
  * Handles requests for the application home page.
@@ -21,6 +33,27 @@ import com.mycompany.calendar.dao.EventDao;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	//Author: 2008160006 강은서
+	
+	//@Qualifier("userService")
+	@Autowired
+	CalendarUserDao calendarUserDao;
+	
+	@Autowired
+	EventDao eventDao;
+	
+	@Autowired
+	EventAttendeeDao eventAttendeeDao;
+	
+	private CalendarUser[] calendarUsers = null;
+	private Event[] events = null;
+	private EventAttendee[] eventAttendees = null;
+	
+	private static final int numInitialNumUsers = 12;
+	private static final int numInitialNumEvents = 4;
+	private static final int numInitialNumEventAttendees = 12;
+	
+	private Random random = new Random(System.currentTimeMillis());
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -35,37 +68,67 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		if(calendarUsers == null && events == null && eventAttendees == null)
+			setUp();
+		List<CalendarUser> listCalendarUsers = calendarUserDao.findAllusers();
+		List<Event> listEvents = eventDao.findAllEvents();
+		List<Date> datelist = new ArrayList<Date>();
 		
-		return "home";
+		model.addAttribute("calendarUsers", listCalendarUsers);		
+		model.addAttribute("events", listEvents);
+		model.addAttribute("eventAttendees", eventAttendees);
+		
+		return "home";				
 	}
 	
-	@RequestMapping(value = "/calendarUsers", method = RequestMethod.GET)
-	public String calendarUsers(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public void setUp() {
+		calendarUsers = new CalendarUser[numInitialNumUsers];
+		events = new Event[numInitialNumEvents];
+		eventAttendees = new EventAttendee[numInitialNumEventAttendees];
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		this.calendarUserDao.deleteAll();
+		this.eventDao.deleteAll();
+		this.eventAttendeeDao.deleteAll();
 		
-		String formattedDate = dateFormat.format(date);
+		for (int i = 0; i < numInitialNumUsers; i++) {
+			calendarUsers[i] = new CalendarUser();
+			calendarUsers[i].setEmail("user" + i + "@example.com");
+			calendarUsers[i].setPassword("user" + i);
+			calendarUsers[i].setName("User" + i);
+			calendarUsers[i].setId(calendarUserDao.createUser(calendarUsers[i]));
+		}
 		
-		model.addAttribute("serverTime", formattedDate );
+		for (int i = 0; i < numInitialNumEvents; i++) {
+			events[i] = new Event();
+			events[i].setSummary("Event Summary - " + i);
+			events[i].setDescription("Event Description - " + i);
+			events[i].setOwner(calendarUsers[random.nextInt(numInitialNumUsers)]);
+			events[i].setEventLevel(EventLevel.NORMAL);
+			switch (i) {				          /* Updated by Assignment 3 */
+				case 0:
+					events[i].setNumLikes(0);  							
+					break;
+				case 1:
+					events[i].setNumLikes(9);
+					break;
+				case 2:
+					events[i].setNumLikes(10);
+					break;
+				case 3:
+					events[i].setNumLikes(10);
+					break;
+			}
+			events[i].setId(eventDao.createEvent(events[i]));
+		}
 		
-		return "calendarUsers";
+		for (int i = 0; i < numInitialNumEventAttendees; i++) {
+			eventAttendees[i] = new EventAttendee();
+			eventAttendees[i].setEvent(events[i % numInitialNumEvents]);
+			eventAttendees[i].setAttendee(calendarUsers[i]);
+			eventAttendees[i].setId(eventAttendeeDao.createEventAttendee(eventAttendees[i]));
+		}
 	}
 	
-	@RequestMapping(value = "/events", method = RequestMethod.GET)
-	public String events(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "events";
-	}
 	
 }
 
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,20 +1,154 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page session="false"%>
+<%@ page import = "java.util.*" %>
+<%@ page import = "java.text.SimpleDateFormat" %>
+<%@ page import = "com.mycompany.calendar.domain.Event" %>
+
 <html>
 <head>
-	<title>Home</title>
+<title>Home</title>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+<script	src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+<style>
+th {
+	bod	
+}
+
+h1 {
+	padding:10px;
+}
+.headline {
+	color:red;
+	
+}
+
+hr {
+	color:black;
+}
+</style>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
-
-<P>  The time on the server is ${serverTime}. </P>
-<p><h1>calendarUsers 내용 출력</h1></p>
-
-<p><h1>events 내용 출력</h1></p>
-<p><h1>eventAttentees</h1></p>
+	<h1>
+	웹 컴퓨팅 및 서비스 4번째 과제 <br/>
+	2008160006 강은석</h1>
+	
+	<p></p>
+	<p></p>
+	<hr size="5" color="blue">
 
 
+	<div>
+		<ul>
+			<c:forEach var="user" items="${calendarUsers}">
+
+			</c:forEach>
+		</ul>
+	</div>
+	<h1 class="headline">calendarUsers 내용 출력</h1>
+	<div>
+		<table class="table table-bordered table-hover">
+			<caption>calendarUsers</caption>
+			<thead>
+			<tr class="danger">
+				<th>ID</th>
+				<th>email</th>
+				<th>password</th>
+				<th>name</th>
+			</tr>
+			</thead>
+			<tbody>
+			<c:forEach var="calendarUser" items="${calendarUsers}">
+				<tr class="info">
+					<td>${calendarUser.id}</td>
+					<td>${calendarUser.email}</td>
+					<td>${calendarUser.password}</td>
+					<td>${calendarUser.name}</td>
+				</tr>
+			</c:forEach>
+			</tbody>
+		</table>
+	</div>
+	<br>
+	<c:set var="eventstest">${events}</c:set>
+	<h1 class="headline">events 내용 출력</h1>
+	<div>
+		<table class="table table-bordered table-hover">
+			<caption>events</caption>
+			<thead>
+			<tr class="danger">
+				<th>id</th>
+				<th>when</th>
+				<th>summary</th>
+				<th>description</th>
+				<th>owner</th>
+				<th>numLikes</th>
+				<th>eventLevel</th>
+			</tr>
+			</thead>
+			<tbody>
+			<c:forEach var="event" items="${events}">
+				<%-- <fmt:formatDate value="${event.when}" type="date" dateStyle="short" /> --%>				
+				<tr class="info">
+					<td>${event.id}</td>
+					<td><fmt:formatDate value="${event.when.time}" type="date" dateStyle="full" /></td>
+					<td>${event.summary}</td>
+					<td>${event.description}</td>
+					<td>${event.owner}</td>
+					<td>${event.numLikes}</td>
+					<td>${event.eventLevel}</td>
+				</tr>
+			</c:forEach>
+			</tbody>
+		</table>
+	</div>
+
+	<h1 class="headline">eventAttentees 출력</h1>
+	<div>
+		<table class="table table-bordered table-hover">
+		<caption>eventAttentees</caption>
+		<thead>
+			<tr class="danger">
+				<th>id</th>
+				<th>event</th>
+				<th>attendee</th>
+			</tr>
+		</thead>
+
+			<c:forEach var="eventAttentee" items="${eventAttendees}">
+				<tr class="info">
+					<td>${eventAttentee.id}</td>
+					<td>${eventAttentee.event}</td>
+					<td>${eventAttentee.attendee}</td>
+				</tr>
+			</c:forEach>
+		</table>
+	</div>
+	<!-- 
+	private Integer id;
+    private Event event;
+    private CalendarUser attendee;
+	 -->
+	
+	<!--  
+    private Integer id;
+    private Calendar when;
+    private String summary;
+    private String description;
+    private CalendarUser owner;
+    private int numLikes;                     /* Updated by Assignment 3 */
+    private EventLevel eventLevel;            /* Updated by Assignment 3 */
+-->
+	<!--  
+ 	private Integer id;
+    private String email;
+    private String password;
+    private String name;
+-->
 </body>
 </html>
